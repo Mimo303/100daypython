d7d899ac30a59c1448605355709c5ac914c055c4
Mimo303 <momitaid606@gmail.com>
2025-07-06 20:55:34 +0530

    Create generatestory.py

---
+++ generatestory.py
@@ -0,0 +1,24 @@
+# Take some input from the user and genearte a story based on that input 
+# you can generate any kind of one but i am generating  this 
+
+print(
+    """hello player , welcome to the world of your story , so i'll ask few questions first and then i make the charcter you always wanted to be , so lets start """
+)
+
+name = input("what is your name ? ")
+worst_enemy = input("who is your worst enemy ? ")
+best_friend = input("who is your best friend ?")
+food = input("what is your fav food ? ")
+wanna_go = input("where do you want to go? ")
+
+print("thanks buddy for the info , I'll make you happy .... wait 😏😜 ")
+
+print(
+    """HI 🤗, I am """, name,
+    """and today that stupid inscure good for nothing  being 🤣 🙂 """,
+    worst_enemy,
+    """try to put me down to their level  🙂but , you know what i did i straight head to my bestie 🫦""",
+    best_friend,
+    """and we enjoy bitching about them with their 😉😊 bestfriend  eating my fav food 🤤😛""",
+    food, """in the best place i could think of which is """, wanna_go,
+    """ and hell yeah he listen all that with his good for nothing ego 🫢😘""")
