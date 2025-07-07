3a3079e1f85c563ffd843610f63a150bef6462f8
Mimo303 <momitaid606@gmail.com>
2025-07-07 17:27:58 +0530

    Create guesscharacter.py

---
+++ guesscharacter.py
@@ -0,0 +1,20 @@
+# Day 5 Challenge python ( replit )
+
+print("Marvel Movie Charater Creator ")
+print("selct Y for yes and N for no ")
+
+
+first_question=input("Do you like hanging around?(no/yes) ")
+if first_question =="Y":
+  print("OH!!! then you are  spiderman 😏 ")
+
+second_question=input("are you green (no/yes)")
+if second_question=="Y":
+  print("ohh hulk  huh  😊🤗!!!")
+
+third_question=input("are you a genius with suit (no/yes) ")
+if third_question=="Y":
+  print(" ahhhh!!! iron man  , i mean i love him too you know huh~~~~ 😘")
+
+else:
+  print(" Guess you are not a avenger fan hmmmm , okay thanks for  playing 🥹 ")
