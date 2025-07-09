df905ac603af84419b58a9e7a1bce804897f9703
Mimo303 <momitaid606@gmail.com>
2025-07-09 13:40:07 +0530

    Create loginsystem.py

---
+++ loginsystem.py
@@ -0,0 +1,10 @@
+# creating a baisc login system using , if , elif and else statememts
+print("creating a basic login system ")
+username=input("enter your username: ")
+password=input("enter the password: ")
+if username=="mark" and password=="1234":
+    print("welcome back mark!")
+elif username=="emily" and password=="5678":
+    print("2nd admin login successful")
+else:
+    print("invalid username or password, please try again")
