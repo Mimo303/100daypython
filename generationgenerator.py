393c3801a4d28b5e7c4ec305c2dfe4cb7a027262
Mimo303 <momitaid606@gmail.com>
2025-07-11 08:46:06 +0530

    Create generationgenerator.py
    Mini project no.9
    It will generate what generation you belong and with a little message

---
+++ generationgenerator.py
@@ -0,0 +1,21 @@
+#generation generator
+name=input("enter your name( 🫠  ): ")
+born_year=int(input("enter the year of your birth( 😏  ): "))
+if born_year>=1925 and born_year<=1946:
+    print("Hello", name ,",you are a Traditionalist")
+    print("Traditionalists value loyalty and discipline.(✿◕‿◕✿)")
+elif born_year>=1947 and born_year<=1964:
+    print("Hello", name , "you are a Baby Boomer")
+    print("Baby Boomers are known for their strong work ethic ლ(╹◡╹ლ).")
+elif born_year>=1965 and born_year<=1981:
+    print("Hello", name , "you are a Generation X")
+    print("Gen X is famous for independence and adaptability.(^◕.◕^)")
+elif born_year>=1982 and born_year<=1995:
+    print("Hello" , name , " You are a Millenial")
+    print("Millenials love technology and collaboration. ^_+")
+elif born_year>=1996 and born_year<=2015:
+    print("YO!", name , ", You belong to Generation Z")
+    print("Gen Z is creative and digitally connected.(￣y▽￣)╭ Ohohoho.....")
+else:
+    print("I am not sure about this generation yet !!🤔 ")
+    print("Maybe you are a pioneer of a new generation!")
