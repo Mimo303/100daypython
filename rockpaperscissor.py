1e029b9a46bf625d05ac6c699bd7b72e1b70ce5d
Mimo303 <momitaid606@gmail.com>
2025-07-12 10:47:24 +0530

    RockPaperScissor.py
    Cool rock paper scissor

---
+++ rockpaperscissor.py
@@ -0,0 +1,39 @@
+# Rock , paper , scissor game 
+from getpass import getpass as input  # its hide your input 
+print("""Welcome to Rock , Paper , Scissor game
+Select R---- Rock 
+Select P------Paper
+Select S------Scissor""")
+player1_name = input("Enter Player 1 name: ")
+print(f"Player 1 name is: {player1_name}")
+player2_name = input("Enter Player 2 name: ")
+print(f"Player 2 name is: {player2_name}")
+player1 = input(f"{player1_name} : Your turn(R/P/S) :  ")
+player2 = input(f"{player2_name} : Your Turn(R/P/S) :  ")
+emoji = {"r": "🪨", "p": "📄", "s": "✂️"}
+
+player1_choice = player1.lower()
+player2_choice = player2.lower()
+
+if player1_choice == player2_choice:
+    print(f"TIE! Both selected {emoji.get(player1_choice, '')}")
+elif player1_choice == "r" and player2_choice == "s":
+    print(f"{player1_name} selects Rock {emoji['r']}, {player2_name} selects Scissor {emoji['s']}")
+    print(f"The winner is : {player1_name}")
+elif player1_choice == "s" and player2_choice == "p":
+    print(f"{player1_name} selects Scissor {emoji['s']}, {player2_name} selects Paper {emoji['p']}")
+    print(f"The winner is : {player1_name}")
+elif player1_choice == "p" and player2_choice == "r":
+    print(f"{player1_name} selects Paper {emoji['p']}, {player2_name} selects Rock {emoji['r']}")
+    print(f"The winner is : {player1_name}")
+elif player2_choice == "r" and player1_choice == "s":
+    print(f"{player1_name} selects Scissor {emoji['s']}, {player2_name} selects Rock {emoji['r']}")
+    print(f"The winner is : {player2_name}")
+elif player2_choice == "s" and player1_choice == "p":
+    print(f"{player1_name} selects Paper {emoji['p']}, {player2_name} selects Scissor {emoji['s']}")
+    print(f"The winner is : {player2_name}")
+elif player2_choice == "p" and player1_choice == "r":
+    print(f"{player1_name} selects Rock {emoji['r']}, {player2_name} selects Paper {emoji['p']}")
+    print(f"The winner is : {player2_name}")
+else:
+    print("Invalid input")
