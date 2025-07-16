22f64a28c427561487a2feae31d5d8f5bf64cb46
Mimo303 <momitaid606@gmail.com>
2025-07-16 09:57:57 +0530

    Update loancalculator(19thday).py
    #corrected one

--- loancalculator(19thday).py
+++ loancalculator(19thday).py
@@ -4,11 +4,11 @@
 amount=int(input("borrowing amount: "))
 apr=float(input("enter the interest rate(give it in decimal): "))
 time=int(input("enter the momentary period(only in year): "))
-for time in range(time):
-  amount+=amount*0.05*time
+for time in range(1,time+1):
+  amount+=amount*apr
   print("-----------------------------------------------")
   print(amount , "is the amount after", time, "year"  "$_$")
   print("-----------------------------------------------")
   print()
-  if time==9:
+  if time==10:
     print(">>>>Your total amount after 10 years is ", amount )
