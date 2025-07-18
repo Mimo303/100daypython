bb2d4fc3375dae95bc853a848f085ad13b985ad1
Mimo303 <momitaid606@gmail.com>
2025-07-18 10:20:11 +0530

    tabletest.py

--- tabletest.py
+++ tabletest.py
@@ -1,11 +1,12 @@
 #test yourself how much you know table 
+table_name=int(input("enter the table number : "))
 score=0
 for i in range(1,11):
   
   if table_name*i==int(input(f"enter the value of {table_name}*{i}: ")):
     score+=1
   else:
-    print()
+    print("nope the answer is", table_name*i)
 
 if score==10:
   print("woooowww, guess you learned your table well 🍫🍫🍫🍫🍫🍫")
